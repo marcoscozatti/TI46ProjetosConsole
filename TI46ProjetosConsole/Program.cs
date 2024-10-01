1497fb3b8d5c2221a1d77b54e5ab834f68299c51
MARCOS ADRIANO FARIA COZATTI <marcos.afcozatti@senacsp.edu.br>
2024-09-30 22:25:50 -0300

    primeira conversao dos programas

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -10,10 +10,21 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("olá mundo");
-            Console.WriteLine("Teste, linha 2");
-            Console.WriteLine("olá mundo");
-            Console.WriteLine("Teste, linha 2");
+            decimal n1, n2, n3, n4, result;
+            string nome;
+            Console.WriteLine("Digite seu nome: \n");
+            nome = Console.ReadLine();
+            Console.WriteLine("Entre com as notas \n");
+            n1 = decimal.Parse(Console.ReadLine());
+            n2 = decimal.Parse(Console.ReadLine());
+            n3 = decimal.Parse(Console.ReadLine());
+            n4 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultado: \n");
+            result = (n1 + n2 + n3 + n4) / 4;
+
+            Console.WriteLine("O aluno " + nome + "teve a média de: " + result);
+        
         }
     }
 }
